3507251fee278e9cc1f2e8d8218898b09a7dc395
giegeralexandra <giegeralexandra@gmail.com>
2021-01-31 15:19:27 -0500

    commit tree daa57f601e41cd3006b7ca70121d05ecc28095b4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(nameArray, eventName) {
+    let newArray = [] 
+    for (let i=0; i < nameArray.length; i++) {
+    newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) { 
+    let i = num; 
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+    
+}
